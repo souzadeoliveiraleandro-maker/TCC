f9ed2eddeb30a192c003b001e9aad254dcd81104
Leandro <souzadeoliveiraleandro@gmail.com>
2025-10-10 19:59:34 -0300

    nova atualização

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import Feed from './src/screens/Feed';
+import Navigator from './src/Navigator'
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => Feed);
